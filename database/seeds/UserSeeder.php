76a1d48e91e7ea21f01e94f111b89e54bf2b8ed5
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-10-24 17:32:23 +0000

    Seed the users table

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+         $users = $this->table('users');
+         $users->truncate();
+         
+         $faker = Faker\Factory::create();
+         $data = [];
+         for ($i = 0; $i < 100; $i++) {
+            $data[] = [
+                'name'      => $faker->name,
+                'email'         => $faker->email,
+                'password'      => sha1($faker->password),
+            ];
+         }
+         $this->insert('users', $data); 
+    }
+}
